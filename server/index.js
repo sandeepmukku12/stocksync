40330b8e76bd26031abb73da5de16904497d3e0b
sandeepmukku <sandeep12akp@gmail.com>
2026-01-15 03:31:51 +0530

    Configure backend for render and Add CORS

--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ dotenv.config();
 const app = express();
 
 // MIDDLEWARE
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://stocksync-iota.vercel.app/",
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
